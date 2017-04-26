b3f9675e802c5c82fd263bf57e54263dd47faf46
Rutger Farry <rutgerfarry@gmail.com>
2017-04-26 13:57:51 -0700

    Remove .min file and add cmd- line tool `index.js`

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-/* @flow */
-module.exports = require("./simple-markdown.js");
+const SimpleMarkdown = require('./simple-markdown')
+const fs = require('fs')
+
+if (process.argv.length > 2) {
+  const filename = process.argv[2]
+
+  fs.readFile(filename, 'utf8', function (err, data) {
+    if (err) throw err
+
+    const mdParse = SimpleMarkdown.defaultBlockParse
+    const mdOutput = SimpleMarkdown.defaultOutput
+
+    const syntaxTree = mdParse(data)
+    const reactElems = mdOutput(syntaxTree)
+    console.log(JSON.stringify(syntaxTree, null, 4))
+  })
+}
